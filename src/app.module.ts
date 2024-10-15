aa4661f56a2298008f0aee9b0964a6f993c3b5da
Yaroslav781 <pelexach12@gmail.com>
2024-10-15 21:21:37 +0300

    added webhook controller and websocket gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import config from './ormconfig';
+import { WebhookController } from './webhook/webhook.controller';
+import { WebsocketGateway } from './websocket.gateway';
 
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(config)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, WebhookController],
+  providers: [AppService, WebsocketGateway],
 })
 export class AppModule {}
